f213cb5cb225334c063dbe3eb176d61feae7e3ee
Yoonvey <3084882664@qq.com>
2018-07-25 10:09:31 +0800

    Commit AppDelegate

---
+++ YVPopupObserver/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YVPopupObserver
+//
+//  Created by Yoonvey on 2018/7/25.
+//  Copyright © 2018年 Yoonvey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
